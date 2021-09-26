864779b2acc22cd970811fb757c4501a200cc0bb
imkevinliao <imkevinliao@gmail.com>
2021-09-26 23:14:11 +0800

    .

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
-cd public/ && git init && git remote add origin git@github.com:imkevinliao/blog.git && git pull origin public
+cd public/ && git init && git remote add origin git@github.com:imkevinliao/imkevinliao.github.io.git && git pull origin master:public
+
+
