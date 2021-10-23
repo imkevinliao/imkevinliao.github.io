c8b792685d756c3f00baa32839c6f509f3c59284
imkevinliao <im.kevinliao@gmail.com>
2021-10-23 21:12:25 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,9 @@ git push origin master:public
 # 第一个项目仓库，第二个网站仓库
 # git push -u origin master:master -f
 # git push -u origin master:public -f
+
+
+# 如果Git提示下面的问题，参考下面的命令
+# The file will have its original line endings in your working directory
+# git config --global core.autocrlf true
+# git config --global core.safecrlf true
