9675b06e17c4a22532c5c2639c3f54e93719c607
imkevinliao <imkevinliao@gmail.com>
2022-02-12 21:41:54 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git push origin master:public
 # 第一次部署出错请使用下面的命令:
 # 第一个项目仓库，第二个网站仓库
 # git push -u origin master:master -f
-# git push -u origin master:public -f
+# public目录下：git push -u origin master:public -f
 
 
 # 如果Git提示下面的问题，参考下面的命令
