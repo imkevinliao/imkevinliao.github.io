2f82f086d768e00bc38e34ed503f994a3605b736
imkevinliao <imkevinliao@gmail.com>
2022-10-23 23:04:34 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,13 @@ if [ ! -n "$msg" ]; then
 fi
 
 echo "===master==="
-hugo  && git add .  && git commit -m "$commit"  && git push
+hugo  && git add .  && git commit -m "$msg"  && git push
 echo "===master==="
 
 echo "===public==="
 cd public/ && git add . && git commit -m "." 
-git push origin master:public
+git push origin master:public --force
 echo "===public==="
-
-# 第一次部署出现错误时候，强制推送，master分支为项目分支，public分支为hugo生成后的网站
 
 # 项目分支：git push -u origin master:master -f
 # 网站分支：cd public/ && git push -u origin master:public -f
