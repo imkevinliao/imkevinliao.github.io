26eb009ff28c20ad97b166beb1f780ac5038992a
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2022-10-24 10:05:15 +0800

    Update deploy.sh
    update deploy, fix bug continue

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,21 @@ if [ ! -n "$msg" ]; then
     msg="."
 fi
 
+# 如果存在public则删除该文件夹
+dir=public
+if [ -d "$dir" ]; then
+    rm -rf $dir
+fi
+
 echo "===master==="
 hugo  && git add .  && git commit -m "$msg"  && git push
 echo "===master==="
 
 echo "===public==="
-cd public/ 
-git add . && git commit -m "." && git push origin master:public --force
+cd public/ && git init && git remote add origin git@github.com:imkevinliao/imkevinliao.github.io.git
+git commit -am "." && git push origin master:public --force
 echo "===public==="
 
-# 项目分支：git push -u origin master:master -f
-# 网站分支：cd public/ && git push -u origin master:public -f
 
 # The file will have its original line endings in your working directory
 # git config --global core.safecrlf true
