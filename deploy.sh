51c9a1122cc18ace4e0712bcb163dc02268e37e7
imkevin <imkevin>
2025-02-02 20:25:56 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,10 @@ if [ -d "$dir" ]; then
     rm -rf $dir
 fi
 
-
 hugo  && git add .  && git commit -m "$msg"  && git push
 echo "************************************************** master branch push okay **************************************************"
 
 git config --global init.defaultBranch master
-
 cd public/ && git init && git remote add origin git@github.com:imkevinliao/imkevinliao.github.io.git
 git add . && git commit -m "." && git push origin master:public --force
 echo "************************************************** public branch push okay **************************************************"
